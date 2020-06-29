b2796424542925b90e2dab7c8fae0834bfe394c0
vfa.giangnt <giangnt@vitalify.asia>
2020-06-29 16:25:10 +0700

    Spring Core: hello world and scope singleton

---
+++ src/gau/nau/HelloWorld.java
@@ -0,0 +1,13 @@
+package gau.nau;
+
+public class HelloWorld {
+private String message;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void getMessage() {
+		System.out.println("Your message: " + message);
+	}
+}
