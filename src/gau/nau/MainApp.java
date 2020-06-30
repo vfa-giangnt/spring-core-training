04c400331c9eb575a54ba98f41228352dd2d86d9
vfa.giangnt <giangnt@vitalify.asia>
2020-06-30 09:26:05 +0700

    Code clean up

--- src/gau/nau/MainApp.java
+++ src/gau/nau/MainApp.java
@@ -6,14 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
     public static void main(String[] args) {
 
-        // Example of inject by constructor
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        HelloWorld objA = (HelloWorld) context.getBean("helloWorldConstructor");
-        objA.print();
-
-        // Example of inject by setter
-        HelloWorld objB = (HelloWorld) context.getBean("helloWorldSetter");
-        objB.print();
 
     }
 
